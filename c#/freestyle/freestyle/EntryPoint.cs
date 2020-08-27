279539d31c2baa356b567696acbac0f367bcb0cf
jideabdqudus <jideabdqudus@gmail.com>
2020-08-27 08:00:47 +0100

    Added Commit into C#

--- c#/freestyle/freestyle/EntryPoint.cs
+++ c#/freestyle/freestyle/EntryPoint.cs
@@ -3,9 +3,36 @@ class EntryPoint
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
-        Console.WriteLine();
-        Console.WriteLine("First C# Code");
-        
+        // Console.WriteLine("Introduction to Programming");
+        // Console.WriteLine();
+        // Console.WriteLine();
+        // Console.WriteLine("Abdul-Qudus");
+        // Console.WriteLine();
+        // Console.WriteLine("jideabdqudus@gmail.com");
+        // Console.WriteLine();
+        // Console.WriteLine("22");
+        // Console.WriteLine();
+        // Console.WriteLine("Male");
+        //
+        // int number0 = 0;
+        // int number178 = 178;
+        // int number21 = -21;
+         int number = 12 ^ 20;
+
+        double numberExp = Math.Pow(number, 20);
+        //
+        // Console.WriteLine(number0);
+        // Console.WriteLine(number178);
+        // Console.WriteLine(number21);
+         Console.WriteLine(numberExp);
+
+        Console.WriteLine(Math.Cos(Math.PI));
+
+        Console.WriteLine(Math.Sqrt(9));
+
+        Console.WriteLine(Math.Round(15.8));
+
+        Console.ReadLine();
+
     }
 }
